8ade182be1527cd5a338c6ce7c19dbc287d7b9fa
Angel Trinh <trinh.angel@gmail.com>
2016-09-10 19:54:09 -0400

    added volunteer section

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if(process.env.JAWSDB_URL) {
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'travelfernweh_db'
+        database: 'unteer_db'
     });
     console.log('connected to local database');
 }
---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+
+CREATE DATABASE unteer_db;
+USE unteer_db;
+
+
+CREATE TABLE users
+(
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  password_hash varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
